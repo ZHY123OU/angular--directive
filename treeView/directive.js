e4f3bc3d5cfac2c57ed5f49994cb282becd8c240
zhouyou <496886668@qq.com>
2017-07-27 14:32:32 +0800

    删掉了一些多余的directive

--- treeView/directive.js
+++ treeView/directive.js
@@ -15,24 +15,3 @@ app.directive('treeView', [function() {
         }]
     }
 }])
-
-.directive('butPos', [function() {
-	
-	return {
-		restrict: 'A',
-		link: function($element) {
-			var eleWidth = $element.width();
-			var eleHeight = $element.height();
-			
-			var childDom = $element.children('div');
-			var childWidth = childDom.width();
-			var childHeight = childDom.height();
-			
-			$element.CSS({
-				'position': 'absolute',
-				'bottom': -$element.height(),
-				'left': -childDom.width()
-			})
-		}
-	}
-}])
